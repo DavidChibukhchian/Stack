891a7337c127d6c3a4729dd45e1423598b756295
DavidChibukhchian <111795181+DavidChibukhchian@users.noreply.github.com>
2022-10-22 18:18:46 +0300

    Update main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -5,25 +5,18 @@ int main()
     Stack stk1 = {};
 
     stackCtor(&stk1, 2);
-    stackDisplay(&stk1);
 
     stackPush(&stk1, 7);
-
     stackPush(&stk1, 8);
-    stackDisplay(&stk1);
-
     stackPush(&stk1, 9);
-    stackDisplay(&stk1);
 
     elem_t top1 = 0;
     stackTop(&stk1, &top1);
+    
+    stackPop(&stk1);
 
     elem_t top2 = 0;
-    stackPop(&stk1);
-    stackDisplay(&stk1);
-
     stackPop(&stk1, &top2);
-    stackDisplay(&stk1);
 
     stackDtor(&stk1);
 
